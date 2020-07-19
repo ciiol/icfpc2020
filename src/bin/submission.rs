8828763cc58674b7e495978d91b9aeb2cd19e95a
Roman Pushkov <pushkovroman@me.com>
2020-07-19 10:49:01 +0300

    Add START request handling

--- src/bin/submission.rs
+++ src/bin/submission.rs
@@ -1,6 +1,8 @@
-use icfpc2020::modulator::{modulate, Modulatable};
+use icfpc2020::modulator::{demodulate, modulate, Modulatable};
 use std::env;
 use std::io::Error;
+use std::str::FromStr;
+use isahc::prelude::*;
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
@@ -8,16 +10,23 @@ fn main() -> Result<(), Error> {
     let server_url = &args[1];
     let player_key = &args[2];
 
+    let player_key_int = i64::from_str(player_key).unwrap();
+
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
 
     let uri = build_api_uri(&server_url);
 
-    let join_request = build_join_request(&player_key);
-    let join_response = isahc::post(uri, join_request)?;
-
-    println!("Response: {:?}", join_response.body());
+    let join_request = build_join_request(player_key_int);
+    let mut join_response = isahc::post(&uri, join_request)?;
 
     assert!(join_response.status().is_success());
+    println!("JOIN Response: {:?}", demodulate(&join_response.text()?.to_string()));
+
+    let start_request = build_start_request(player_key_int);
+    let mut start_response = isahc::post(&uri, start_request)?;
+
+    assert!(start_response.status().is_success());
+    println!("START Response: {:?}", demodulate(&start_response.text()?.to_string()));
 
     Ok(())
 }
@@ -27,14 +36,26 @@ fn build_api_uri(server_url: &str) -> String {
     format!("{}/aliens/send?apiKey={}", server_url, api_key)
 }
 
-fn build_join_request(player_key: &str) -> String {
-    let player_key_int = player_key.parse::<i64>().unwrap();
-
+fn build_join_request(player_key: i64) -> String {
     modulate(&Modulatable::Pair(
         Box::new(Modulatable::Num(2)),
         Box::new(Modulatable::Pair(
-            Box::new(Modulatable::Num(player_key_int)),
+            Box::new(Modulatable::Num(player_key)),
             Box::new(Modulatable::Pair(
+                Box::new(Modulatable::Nil),
+                Box::new(Modulatable::Nil),
+            )),
+        )),
+    ))
+}
+
+fn build_start_request(player_key: i64) -> String {
+    modulate(&Modulatable::Pair(
+        Box::new(Modulatable::Num(3)),
+        Box::new(Modulatable::Pair(
+            Box::new(Modulatable::Num(player_key)),
+            Box::new(Modulatable::Pair(
+                // TODO: add ship characterstics
                 Box::new(Modulatable::Nil),
                 Box::new(Modulatable::Nil),
             )),
